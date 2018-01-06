076e28cb69c0af731893e59ed449c9c49ce4029e
Melike Aydin <melikeaydin430@gmail.com>
2018-01-06 17:32:22 +0300

    Update asama1.aspx.cs

--- asama1.aspx.cs
+++ asama1.aspx.cs
@@ -31,51 +31,6 @@ namespace yazlabYam1
             string icerik2 = result.Code;
 
             string arananKelime = TextBox2.Text;
-
-            //char[] diziTurkce = new char[6];
-            //diziTurkce[0] = 'ı';
-            //diziTurkce[1] = 'ö';
-            //diziTurkce[2] = 'ü';
-            //diziTurkce[3] = 'ş';
-            //diziTurkce[4] = 'ğ';
-            //diziTurkce[5] = 'ç';
-            //char[] diziIngilizce = new char[6];
-            //diziIngilizce[0] = 'i';
-            //diziIngilizce[1] = 'o';
-            //diziIngilizce[2] = 'u';
-            //diziIngilizce[3] = 's';
-            //diziIngilizce[4] = 'g';
-            //diziIngilizce[5] = 'c';
-            //char[] harfler = arananKelime.ToCharArray();
-            //for(int i=0; i<harfler.Length; i++)
-            //{
-            //    if (harfler[i] == diziTurkce[0])
-            //        harfler[i] = diziIngilizce[0];
-            //   else if (harfler[i] == diziTurkce[1])
-            //        harfler[i] = diziIngilizce[1];
-            //    else if (harfler[i] == diziTurkce[2])
-            //        harfler[i] = diziIngilizce[2];
-            //   else  if (harfler[i] == diziTurkce[3])
-            //        harfler[i] = diziIngilizce[3];
-            //  else  if (harfler[i] == diziTurkce[4])
-            //        harfler[i] = diziIngilizce[4];
-            //  else  if (harfler[i] == diziTurkce[5])
-            //        harfler[i] = diziIngilizce[5];
-            //   else if (harfler[i] == diziIngilizce[0])
-            //        harfler[i] = diziTurkce[0];
-            //    else if (harfler[i] == diziIngilizce[1])
-            //        harfler[i] = diziTurkce[1];
-            //    else if (harfler[i] == diziIngilizce[2])
-            //        harfler[i] = diziTurkce[2];
-            //    else if (harfler[i] == diziIngilizce[3])
-            //        harfler[i] = diziTurkce[3];
-            //    else if (harfler[i] == diziIngilizce[4])
-            //        harfler[i] = diziTurkce[4];
-            //    else if (harfler[i] == diziIngilizce[5])
-            //        harfler[i] = diziTurkce[5];
-            //}
-            //string yeni = new string(harfler);
-            //arananKelime = yeni;
          
             Label3.Text = icerik2 + "<br/>" + "sonuc : " + Regex.Matches(icerik2.ToString().ToLower(), arananKelime.ToLower()).Count;
 
